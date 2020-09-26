d69f3ab9d8ef81bb1f57a7fb79012a4f107662d8
Fernando Henrique <liquuid@gmail.com>
2020-09-26 19:49:29 -0300

    style of issue list

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import logoImg from '../../assets/logo.svg';
+
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import { Header, RepositoryInfo, Issues } from './styles';
 
 interface RepositoryParams {
     repository: string;
@@ -8,7 +12,55 @@ interface RepositoryParams {
 const Repository: React.FC = () => {
     const { params } = useRouteMatch<RepositoryParams>();
 
-    return <h1>Repository: {params.repository} </h1>
+    return (
+        <>
+            <Header>
+                <img src={logoImg} alt="Github Explorer" />
+                <Link to="/"><FiChevronLeft size={16} />
+                    Voltar
+                </Link>
+            </Header>
+            <RepositoryInfo>
+                <header>
+                    <img src="" alt="foo" />
+                    <div>
+                        <strong>fooo/bar</strong>
+                        <p>yada yada ada</p>
+                    </div>
+                </header>
+                <ul>
+                    <li>
+                        <strong>9999</strong>
+                        <span>Stars</span>
+                    </li>
+                    <li>
+                        <strong>8888</strong>
+                        <span>Forks</span>
+                    </li>
+                    <li>
+                        <strong>7777</strong>
+                        <span>Issues Abertas</span>
+                    </li>
+                </ul>
+            </RepositoryInfo>
+            <Issues>
+
+
+                <Link to="">
+
+                    <div>
+                        <strong>dddddddddddddd</strong>
+                        <p>ffffffffffffffff</p>
+                    </div>
+                    <FiChevronRight size={20} />
+                </Link>
+
+
+
+            </Issues>
+        </>
+    );
+
 }
 
 export default Repository;
