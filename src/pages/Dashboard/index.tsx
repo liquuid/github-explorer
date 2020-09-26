84be4bff947bc66c2d63d31d1ca99fa2f8475292
Fernando Henrique <liquuid@gmail.com>
2020-09-26 18:27:45 -0300

    getting params

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,8 @@
 import React, { useState , useEffect, FormEvent } from 'react';
 import { Title , Form, Repositories, Error } from './style';
+import { Link } from 'react-router-dom';
 import logoImg from '../../assets/logo.svg';
-import { FiChevronRight } from 'react-icons/fi';
+import { FiChevronRight, FiPrinter } from 'react-icons/fi';
 import api from '../../services/api';
 
 import './style.ts';
@@ -72,14 +73,14 @@ const Dashboard: React.FC = () => {
 
         <Repositories>
             { repositories.map( repository => (
-                <a key={repository.full_name} href="">
+                <Link key={repository.full_name} to={`/repositories/${repository.full_name}`}>
                     <img src={repository.owner.avatar_url} alt={repository.owner.login}/>
                     <div>
                         <strong>{repository.full_name}</strong>
                         <p>{repository.description}</p>
                     </div>
                     <FiChevronRight size={20} />
-                </a>
+                </Link>
             ))}
 
         </Repositories>
